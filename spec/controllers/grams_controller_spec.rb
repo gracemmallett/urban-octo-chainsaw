5a095846fa76b6ac219daedd8c7707d762dc96d8
Grace Mallett <gmmallet@bowdoin.edu>
2017-05-25 18:29:23 +0000

    factory girl

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
@@ -16,11 +17,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
       get :new
       expect(response).to have_http_status(:success)
@@ -33,13 +30,9 @@ RSpec.describe GramsController, type: :controller do
       post :create, gram: { message: "Hello" }
       expect(response).to redirect_to new_user_session_path
     end
-    
+
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
@@ -50,11 +43,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
       post :create, gram: {message: '' }
       expect(response).to have_http_status(:unprocessable_entity)
